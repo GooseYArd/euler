f8dd0425fb8c8b50bae76c98c97f57b7d38f8f1f
Andy Bailey <GooseYArd@gmail.com>
2012-01-02 12:34:29 -0500

    reuse sieve to solve problem 7

---
+++ 7/7.go
@@ -0,0 +1,59 @@
+package main
+import "fmt"
+
+func generate() chan int {
+	ch := make(chan int)
+	go func() {
+		for i := 2; ; i++ {
+			ch <- i
+		}
+	}()
+	return ch
+}
+
+func filter(in chan int, prime int) chan int {
+	out := make(chan int)
+	go func() {
+		for {
+			if i := <-in; i%prime != 0 {
+				out <- i
+			}
+		}
+	}()
+	return out
+}
+
+func sieve() chan int {
+	out := make(chan int)
+	go func() {
+		ch := generate()
+		for {
+			prime := <-ch
+			out <- prime
+			ch = filter(ch, prime)
+		}
+	}()
+	return out
+}
+
+func main() {
+
+	// 
+	// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+	// What is the 10 001st prime number?
+	// 
+	// 
+
+	s := sieve()
+	next := 0
+	for i := 0; i < 10000; i++ {
+		next = <-s
+	}
+
+	next = <-s
+	fmt.Printf("10001: %v\n", next)
+
+}
+
+
+ 
