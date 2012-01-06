a9f6cabe26f94e92f20a4ff4ba976c96f1a5ff2b
Andy Bailey <GooseYArd@gmail.com>
2012-01-06 15:20:22 -0500

    add 15 stub

---
+++ 15/15.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+
+	// 
+	// Starting in the top left corner of a 22 grid, there are 6 routes (without backtracking) to the bottom right corner.
+	// 
+	// 
+	// 
+	// How many routes are there through a 2020 grid?
+	// 
+	// 
+}
+ 
