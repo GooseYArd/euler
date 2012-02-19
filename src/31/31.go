c505d7416dd5e82f3fa36b157706e09dc0e3ec48
Andy Bailey <GooseYArd@gmail.com>
2012-02-19 16:55:15 -0500

    finish 31

--- src/31/31.go
+++ src/31/31.go
@@ -3,23 +3,36 @@ import "fmt"
 
 func combinations(target int, coins []int) (n int) {		
 
+	if target == 0 {
+		return 0
+	}
+	
 	if len(coins) == 0 {
 		return 0
 	}
 	
+	// if only one value of coin is left, and that value divides evenly
+	// into the remaining value, then exactly one combination remains
 	if len(coins) == 1 {
 		if target % coins[0] > 0 {
 			return 0
 		}
 		return 1
 	}
+	
+	for i := 0; i * coins[0] < target; i++ {
+		v := combinations(target - (i * coins[0]), coins[1:])				
+		n+=v
+	}	
 
-	i := 0
-	for ; i * coins[0] < target; i++ {
-		n += combinations(target - (i * coins[0]), coins[1:])
+	// in case i * coins[0] == target
+	if target % coins[0] == 0 {
+		n++
 	}
-	return n + (i-1)
+
+	return n
 }
+
 
 func main() {
 
@@ -34,7 +47,13 @@ func main() {
 	// 
 	
 	coins := []int{200, 100, 50, 20, 10, 5, 2, 1}
-	//coins := []int{200, 100, 50}
+	//coins := []int{200, 100, 50, 20}
+
+	// 200
+	// 100, 100
+	// 100, 50, 50
+	// 50, 50, 50, 50
+
 	target := 200
 	
 	total := combinations(target, coins)
